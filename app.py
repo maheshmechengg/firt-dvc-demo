9dc4c5827351ca72cc39776e2011dd72879fbfd4
Mahesh Chauhan <maheshmechenggi@gmail.com>
2021-07-29 14:15:58 +0530

    added predict

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os
 import joblib
 import numpy as np
 import yaml
-#from prediction_service import prediction
+from prediction_service import prediction
 
 params_path = "params.yaml"
 webapp_root = "webapp"
@@ -13,53 +13,29 @@ template_dir = os.path.join(webapp_root, "templates")
 
 app = Flask(__name__, static_folder=static_dir, template_folder=template_dir)
 
-def read_params(config_path=params_path):
-    with open(config_path) as yaml_file:
-        config = yaml.safe_load(yaml_file)
-    return config
-
-def predict(data):
-    config = read_params(params_path)
-    model_dir_path = config["webapp_model_dir"]
-    model = joblib.load(model_dir_path)
-    #print(data)
-    prediction = model.predict(data)
-    print(prediction)
-    return prediction[0]
-
-def api_response(request):
-    try:
-        data = np.array([list(request.json.values())])
-        response = predict(data)
-        response = {"response": response}
-        return response
-    except Exception as e:
-        print(e)
-        error = {"error": "Some error occured"}
-        return error
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
         try:
             if request.form:
-                dat = dict(request.form)
+                data_req = dict(request.form)
                 #print(dat)
-                a = dat.values()
-                b = list(a)
-                c = np.array(b)
-                d = [list(map(float, c))]
+                #a = dat.values()
+                #b = list(a)
+                #c = np.array(b)
+                #d = [list(map(float, c))]
                 #print(d)
-                respons = predict(d)
-                return render_template("index.html", response=respons)
+                response = prediction.form_response(data_req)
+                return render_template("index.html", response=response)
             elif request.json:
-                respons = api_response(request)
-                return jsonify(respons)
+                response = prediction.api_response(request.json)
+                return jsonify(response)
 
         except Exception as e:
             print(e)
-            error = {"error": "Some error occured"}
-            return render_template("404.html", error=error)
+            #error = {"error": "Some error occured"}
+            return render_template("404.html", error=e)
     else:
         return render_template("index.html")
 
